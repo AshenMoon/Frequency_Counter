e88b91525a6c8129f63236aa6ab0c337565f9631
AshenMoon <152204005+AshenMoon@users.noreply.github.com>
2025-06-05 20:11:56 +0300

    Automatic Gate Selection

--- Frequency_Counter.ino
+++ Frequency_Counter.ino
@@ -7,38 +7,48 @@
   http://www.khm.de
   http://interface.khm.de/index.php/lab/interfaces-advanced/arduino-frequency-counter-library/	
  */
+
+//The Frequency input is fixed to digital pin 5.
+
 #include <FreqCounter.h>
 
 
-unsigned long frq;
+unsigned long frequency;
 int cnt;
-int pinLed=13;
+int pinLed = 13;
 
 void setup() {
   pinMode(pinLed, OUTPUT);
 
-  Serial.begin(115200);        // connect to the serial port
+  Serial.begin(115200);  // connect to the serial port
 
   Serial.println("Frequency Counter");
-
 }
 
-
+int gate = 100;  // 100 ms Gate Time
 
 void loop() {
 
   // wait if any serial is going on
-  FreqCounter::f_comp=10;   // Cal Value / Calibrate with professional Freq Counter
-  FreqCounter::start(100);  // 100 ms Gate Time
+  FreqCounter::f_comp = 10;  // Cal Value / Calibrate with professional Freq Counter
 
-  while (FreqCounter::f_ready == 0) 
+  FreqCounter::start(gate);
 
-  frq=FreqCounter::f_freq;
+  while (FreqCounter::f_ready == 0);
+
+  unsigned int factor = 1000 / gate;
+
+  frequency = FreqCounter::f_freq * factor;
+
   Serial.print(cnt++);
   Serial.print("  Freq: ");
-  Serial.println(frq);
+  Serial.println(frequency);
+
+  if(frequency > 999)
+    gate = 100;
+  else
+    gate = 1000;
+  
   delay(20);
-  digitalWrite(pinLed,!digitalRead(pinLed));  // blink Led
-
-}  
-
+  digitalWrite(pinLed, !digitalRead(pinLed));  // blink Led
+}
